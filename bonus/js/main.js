73e2b5246fad1c16f993f4ed15163a9e547b2fcd
leonardoserra <serra.leonardo92@gmail.com>
2023-02-24 15:53:42 +0100

    preparo cartella bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+
+for(let i = 1; i<=100; i++){
+ 
+    if ((i % 5 === 0) && (i % 3 === 0)){
+        let square = `<div class="square fizzBuzzBg">FIZZBUZZ</div>`;
+        container.innerHTML += square;
+    }else if(i % 3 === 0){
+        let square = `<div class="square fizzBg">FIZZ</div>`;
+        container.innerHTML += square;
+    }else if (i % 5 === 0){
+        let square = `<div class="square buzzBg">BUZZ</div>`;
+        container.innerHTML += square;
+    } else{
+        let square = `<div class="square standardBg">${i}</div>`;
+        container.innerHTML += square;
+    }
+
+}
