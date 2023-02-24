fe8889ef24ff663711f2e91a2051dcf27e413e65
leonardoserra <serra.leonardo92@gmail.com>
2023-02-24 15:23:03 +0100

    esce un quadrato

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const container = document.querySelector('.container');
+let square = document.querySelector('.square');
 
-for(let i = 0; i<=100; i++){
-    let square = `<div>${i}</div>`;
+for(let i = 1; i<=100; i++){
+    container.innerHTML = `<div>${i}</div>`;
+    square.classList.add('standardBg');
     container.innerHTML += square;
+  
     
     if(!(3 % square)){
         square.classList.add('fizzBg');
+        container.innerHTML += square;
     }else if (!(5 % square)){
         square.classList.add('buzzBg');
+        container.innerHTML += square;
     }else if (!(5 % square) && !(3 % square)){
         square.classList.add('fizzBuzzBg');
+        container.innerHTML += square;
     } else{
         square.classList.add('standardBg');
+        container.innerHTML += square;
     }
+
+    
 }
