48e2dda77c3a3926b58a3ec3895ae3aae0b8e2cd
leonardoserra <serra.leonardo92@gmail.com>
2023-02-24 15:45:24 +0100

    escono quadrati sbagliati.. logica if da rivedere

--- js/main.js
+++ js/main.js
@@ -2,24 +2,23 @@ const container = document.querySelector('.container');
 
 
 for(let i = 1; i<=100; i++){
-    let square = `<div class="square">${i}</div>`;
-    
+ 
   
     
-    // if(!(3 % i)){
-    //     square.classList.add('fizzBg');
-        
-    // }else if (!(5 % i)){
-    //     square.classList.add('buzzBg');
-        
-    // }else if (!(5 % i) && !(3 % i)){
-    //     square.classList.add('fizzBuzzBg');
-        
-    // } else{
-    //     square.classList.add('standardBg');
-        
-    // }
+    if(3 % i == 0){
+        let square = `<div class="square fizzBg">${i}</div>`;
+        container.innerHTML += square;
+    }else if (5 % i == 0){
+        let square = `<div class="square buzzBg">${i}</div>`;
+        container.innerHTML += square;
+    }else if ((5 % i == 0) && ( 3 % i) == 0){
+        let square = `<div class="square fizzBuzzBg">${i}</div>`;
+        container.innerHTML += square;
+    } else{
+        let square = `<div class="square standardBg">${i}</div>`;
+        container.innerHTML += square;
+    }
 
-    container.innerHTML += square;
+   
 
 }
